07cd0c19944cd844b394d1fe08f4adfe45174ac5
sagar <sagarmairrajput@gmail.com>
2024-03-18 18:54:32 +0530

    changes

--- src/components/sub-components-header/Address.js
+++ src/components/sub-components-header/Address.js
@@ -3,10 +3,14 @@ import { LOC_URL } from "../../config/constants.js";
 const Address = () => {
   return (
     <div className="header-address">
-      <img className="location-logo" src={ LOC_URL }></img>
-      <span className="receipent-name">Deleiever to Saurabh</span>
-      <span className="state">Varanasi</span>
-      <span className="pin">221003</span>
+      <img className="location-logo" src={LOC_URL}></img>
+      <div className="add-details">
+        <span className="receipent-name">Deleiever to Saurabh</span>
+        <div className="locality">
+          <span className="state">Varanasi</span>
+          <span className="pin">221003</span>
+        </div>
+      </div>
     </div>
   )
 }
