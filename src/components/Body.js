dfebb5e8136f03294fd066428e14cfdf59a33323
sagar <sagarmairrajput@gmail.com>
2024-03-22 01:00:21 +0530

    Added top products filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,13 +6,23 @@ import Category3 from "./sub-components/body/Category3";
 import Category4 from "./sub-components/body/Category4";
 import Category5 from "./sub-components/body/Category5";
 import prodObj from "../config/mockdata.js";
+import { useState } from "react";
 
 const Body = () => {
+  const [ listOfProducts, setProducts ] = useState(prodObj.slice(0, 10));
   return (
     <div className="body-wrapper">
+      <button className="top-products" onClick={() => {
+        console.log(prodObj);
+        const topProducts = listOfProducts.filter(
+          (product) => product.ratingValue > 4.1
+        );
+        setProducts(topProducts);
+      }}> Top Products
+      </button>
       <div className="pick-up">
         <Title data="Pick up where you left off" />
-        { prodObj.slice(0, 4).map(item => <Category1 prodData={item} />) }
+        { listOfProducts.map(item => <Category1 prodData={item} />)}
       </div>
     </div>
   )
